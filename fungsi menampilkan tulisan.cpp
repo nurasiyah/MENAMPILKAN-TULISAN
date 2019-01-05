749dada25c12da62349c3b3ccc9cdd92e6225c51
nurasiyah <45659380+nurasiyah@users.noreply.github.com>
2019-01-05 14:26:07 +0700

    Add files via upload

---
+++ fungsi menampilkan tulisan.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <conio.h>
+using namespace std;
+
+int main()
+{
+    int A;
+    A=1;
+
+    while (A<=30)
+    {
+        cout<<"Algoritma dan Pemrograman"<<endl;
+        A++;
+    }
+    getch();
+}
